aca36baadf18aa8a370e1b0d52b6f6ab60c1ff19
vujadin95 <vujke456@gmail.com>
2024-10-25 15:21:58 +0200

    translating navigation links

--- src/app/components/Header/Navbar/Navbar.tsx
+++ src/app/components/Header/Navbar/Navbar.tsx
@@ -1,15 +1,21 @@
+import { useTranslations } from "next-intl";
 import NavLink from "./NavLink";
-import { navigationLinks } from "@/src/app/data/navigationLinks";
 
 const Navbar = () => {
+  const t = useTranslations('navigationLinks');
+
+  // add all main link names here and in messages (lang).json
+  const linkNames = ['about-us', 'portfolio', 'whySuperheat', 'optimizationSystem', 'contact'];
+
+
   return (
     <nav className="mobile-navigation hidden shrink-0 md:flex items-center ml-auto">
-      {navigationLinks.map((link) => (
+      {linkNames.map((linkName) => (
         <NavLink
           styleClass=" text-sm lg:text-base lg:tracking-wide font-semibold lg:px-3 px-[6px] py-[6px] rounded-sm dark:hover:bg-zinc-600 hover:bg-zinc-200 transition-color duration-150 md:ml-auto"
-          key={link.id}
-          path={link.path}
-          linkLabel={link.linkLabel}
+          key={t(`${linkName}.id`)}
+          path={t(`${linkName}.path`)}
+          linkLabel={t(`${linkName}.label`)}
         />
       ))}
     </nav>
